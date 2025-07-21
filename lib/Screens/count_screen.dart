90fcbd8581661ec466d12d08f6ba3ea135acc66c
SubhanOrax <subahnoraxtech.@gmail.com>
2025-07-21 18:46:51 +0500

    optimized using Consumer

--- lib/Screens/count_screen.dart
+++ lib/Screens/count_screen.dart
@@ -12,6 +12,7 @@ class CountScreen extends StatefulWidget {
 class _CountScreenState extends State<CountScreen> {
   @override
   Widget build(BuildContext context) {
+    // final countprovider = Provider.of<CountProvider>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -24,46 +25,95 @@ class _CountScreenState extends State<CountScreen> {
         ),
         backgroundColor: Colors.teal[600],
       ),
-      body: Center(
-        child: Column(
+
+      /// with the help of cosumer we optimized our app a little bit as it dont need to rebuild the buttons again as we just added them in the child of Consumer so only the text will rebuild
+      body: Consumer<CountProvider>(
+        builder: (context, value, child) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                value.count.toString(),
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              child!,
+            ],
+          ),
+        ),
+
+        ///this will not rebuild
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              context.watch<CountProvider>().count.toString(),
-              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            FloatingActionButton(
+              onPressed: context.read<CountProvider>().addcount,
+              shape: const CircleBorder(),
+              backgroundColor: Colors.teal[600],
+              splashColor: Colors.tealAccent[50],
+              foregroundColor: Colors.white,
+              child: const Icon(Icons.add),
             ),
             const SizedBox(
-              height: 20,
+              width: 10,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: context.watch<CountProvider>().addcount,
-                  shape: const CircleBorder(),
-                  backgroundColor: Colors.teal[600],
-                  splashColor: Colors.tealAccent[50],
-                  foregroundColor: Colors.white,
-                  child: const Icon(Icons.add),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                FloatingActionButton(
-                  onPressed: context.watch<CountProvider>().subcount,
-                  shape: const CircleBorder(),
-                  backgroundColor: Colors.teal[600],
-                  splashColor: Colors.tealAccent[50],
-                  foregroundColor: Colors.white,
-                  child: const Icon(
-                    Icons.remove,
-                  ),
-                ),
-              ],
+            FloatingActionButton(
+              onPressed: context.read<CountProvider>().subcount,
+              shape: const CircleBorder(),
+              backgroundColor: Colors.teal[600],
+              splashColor: Colors.tealAccent[50],
+              foregroundColor: Colors.white,
+              child: const Icon(
+                Icons.remove,
+              ),
             ),
           ],
         ),
-      ),
+      )
+
+      /// this was a direct methode but this does not optimize performace as it rebuilts the other widgets too like buttons which no needed to be rebuild.
+      // child: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Text(
+      //       context.watch<CountProvider>().count.toString(),
+      //       style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      //     ),
+      //     const SizedBox(
+      //       height: 20,
+      //     ),
+      //     Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         FloatingActionButton(
+      //           onPressed: context.watch<CountProvider>().addcount,
+      //           shape: const CircleBorder(),
+      //           backgroundColor: Colors.teal[600],
+      //           splashColor: Colors.tealAccent[50],
+      //           foregroundColor: Colors.white,
+      //           child: const Icon(Icons.add),
+      //         ),
+      //         const SizedBox(
+      //           width: 10,
+      //         ),
+      //         FloatingActionButton(
+      //           onPressed: context.watch<CountProvider>().subcount,
+      //           shape: const CircleBorder(),
+      //           backgroundColor: Colors.teal[600],
+      //           splashColor: Colors.tealAccent[50],
+      //           foregroundColor: Colors.white,
+      //           child: const Icon(
+      //             Icons.remove,
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ],
+      // ),
+      ,
     );
   }
 }
